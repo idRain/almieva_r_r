f98bc77a932339936def32dcf8035e17cf366526
ralina <almieva.ralina@yandex.ru>
2022-05-23 15:04:14 +0300

    change kr

--- prj.labs/kr/kr.cpp
+++ prj.labs/kr/kr.cpp
@@ -1,8 +1,8 @@
 #include <opencv2/opencv.hpp>
 
 cv::Mat makePicture() {
-  std::vector<int> rect_colors = { 0, 127, 250, 127, 250, 0 };
-  std::vector<int> circle_colors = { 127, 0, 127, 250, 0, 250 };
+  std::vector<int> rect_colors = { 0, 127, 255, 127, 255, 0 };
+  std::vector<int> circle_colors = { 127, 0, 127, 255, 0, 255 };
   cv::Mat picture(300, 450, CV_8UC1);
   picture = 0;
 
@@ -17,7 +17,7 @@ cv::Mat makePicture() {
   int r = 60;
   for (int i = 0; i < 2; i++) {
     for (int j = 0; j < 3; j++) {
-      cv::circle(picture, cv::Point(j * 2 * 75 + 75 , i * 2 * 75 + 75), r, circle_colors[i * 3 + j], -1);
+      cv::circle(picture, cv::Point(j * 2 * 75 + 75, i * 2 * 75 + 75), r, circle_colors[i * 3 + j], -1);
     }
   }
 
@@ -25,19 +25,33 @@ cv::Mat makePicture() {
 }
 
 void applyFilter(cv::Mat& src, cv::Mat& dst, cv::Mat& kernel) {
-  cv::filter2D(src, dst, -1, kernel, cv::Point(1, 1));
+  cv::Mat current_dst;
+  src.convertTo(current_dst, CV_32FC1, 1.0 / 255);
+  cv::filter2D(current_dst, current_dst, -1, kernel, cv::Point(1, 1));
+  cv::normalize(current_dst, current_dst, 1, 0, cv::NORM_MINMAX);
+  current_dst.convertTo(dst, CV_8UC1, 255);
 }
 
 //create mat equals to geometrical mean of two mats
 void makeGeometricMean(cv::Mat& dst, cv::Mat& m1, cv::Mat& m2) {
+  cv::Mat mat1;
+  cv::Mat mat2;
+  cv::Mat current_dst;
+  m1.convertTo(mat1, CV_32FC1, 1.0 / 255);
+  m2.convertTo(mat2, CV_32FC1, 1.0 / 255);
+  dst.convertTo(current_dst, CV_32FC1, 1.0 / 255);
   int cols = m1.cols;
   int rows = m1.rows;
   for (int i = 0; i < cols; i++) {
     for (int j = 0; j < rows; j++) {
-      dst.at<uchar>(cv::Point(i, j)) = round(sqrt(pow(m1.at<uchar>(cv::Point(i, j)), 2) + pow(m2.at<uchar>(cv::Point(i, j)), 2)));
+      current_dst.at<float>(cv::Point(i, j)) = sqrt(pow(mat1.at<float>(cv::Point(i, j)), 2) + pow(mat2.at<float>(cv::Point(i, j)), 2));
     }
   }
+  cv::normalize(current_dst, current_dst, 1.0, 0.0, cv::NORM_MINMAX);
+  current_dst.convertTo(dst, CV_8UC1, 255);
 }
+
+
 
 int main() {
   //make original picture
@@ -46,7 +60,7 @@ int main() {
 
   //apply 1 filter and make I1
   cv::Mat I1;
-  std::vector<float> k_vector_1 = {1, 0, -1, 2, 0, -2, 1, 0, -1};
+  std::vector<float> k_vector_1 = { 1, -2, 1, 0, 0, 0, 1, -2, 1 };
   cv::Mat kernel_1(3, 3, CV_32FC1);
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
@@ -58,7 +72,7 @@ int main() {
 
   //apply 2 filter and make I2
   cv::Mat I2;
-  std::vector<float> k_vector_2 = { 1, 2, 1, 0, 0, 0, -1, -2, -1 };
+  std::vector<float> k_vector_2 = { 1, 0, 1, -2, 0, 2, 1, 0, 1 };
   cv::Mat kernel_2(3, 3, CV_32FC1);
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
